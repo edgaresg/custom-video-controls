599f898b8e7bd151d9fb060e4e29327c25337e6d
Edgar Montiel <145402671+edgaresg@users.noreply.github.com>
2024-07-12 16:37:26 -0600

    feat: progress bar dynamic

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { MouseEvent, useRef, useState } from 'react'
 
 function formatTime(time: number): string {
   return `${time / 60 < 10 ? Math.floor(time / 60) : Math.floor(time / 60)}:${
@@ -11,11 +11,13 @@ export default function Index () {
   const [playing, setPlaying] = useState<boolean>(false)
   const [duration, setDuration] = useState<string>()
   const [currentTime, setCurrentTime] = useState<string>()
+  const [progressPercent, setProgressPercent] = useState<number>(0)
 
   const onTimeUpdate = () => {
     if (videoRef.current) {
       setCurrentTime(formatTime(videoRef.current.currentTime))
       setDuration(formatTime(videoRef.current.duration))
+      setProgressPercent((videoRef.current.currentTime * 100) / videoRef.current.duration)
     }
   }
 
@@ -29,6 +31,11 @@ export default function Index () {
       setPlaying(false)
     }
   }
+  
+  const handleCurrentTime = (e: MouseEvent<HTMLDivElement>) => {
+    const video = videoRef.current as HTMLVideoElement
+    console.log(e);
+  }
 
   return (
     <main className='max-w-screen-lg mx-auto'>
@@ -40,12 +47,18 @@ export default function Index () {
       >
         <source src='https://videos.pexels.com/video-files/5495322/5495322-hd_1920_1080_30fps.mp4' />
       </video>
-      <div className='flex gap-5'>
+      <div className='flex gap-5 items-center'>
         {playing
           ? <button onClick={handlePlayPauseVideo}>Pause</button>
           : <button onClick={handlePlayPauseVideo}>Play</button>}
-
-        <span>
+          <div className='w-full h-1 bg-neutral-600/40 relative'>
+            <div
+              className='bg-neutral-900 absolute left-0 top-0 z-10 h-1 cursor-pointer' 
+              style={{ width: `${String(progressPercent)}%` }}
+              onClick={handleCurrentTime} 
+            />
+          </div>
+          <span>
           {currentTime}/{duration}
           </span>
       </div>
